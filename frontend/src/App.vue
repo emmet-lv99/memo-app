cd5fdf321feb32ee5390678e9c51e0090593f775
dachanisfull@gmail.com <dachan.kim>
2022-08-11 00:26:52 +0900

    tmp

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,8 +6,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-
-
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
