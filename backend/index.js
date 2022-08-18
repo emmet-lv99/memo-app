388f5155498620b6c31cb350e0fab17c80e9dd0c
emmet-lv99 <dachanisfull@gmail.com>
2022-08-19 07:56:31 +0900

    add axios

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const memos = ['메모1 내용', '메모2 내용', '메모3 내용']
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(memos)
 })
 
 app.listen(port, () => {
